5e3dadadf0f468307c229e2ad67ac611787016ba
yungakumajke <kolsmaunt@gmail.com>
2023-04-24 22:02:12 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import './App.css';
-const tg = window.Telegram.WebApp;
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
